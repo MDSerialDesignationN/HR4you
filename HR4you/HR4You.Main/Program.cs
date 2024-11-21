1cf0aa64e769a646cf6054a3a2ab7f73120a2bb8
Alexander Binder <Alexander.Binder@zh-tech.at>
2024-11-21 14:51:42 +0100

    WIP-Commit for DB-Context and configured API aka Program.cs:
    Moved razor components
    
    Added Swagger for API endpoint testing
    
    Configured MVC services
    
    Replaced default exception handler with status page popup

--- HR4you/HR4You.Main/Program.cs
+++ HR4you/HR4You.Main/Program.cs
@@ -1,31 +1,98 @@
-using HR4You.Components;
+using System.Text.Json.Serialization;
+using HR4You.Blazor.Components;
+using Microsoft.OpenApi.Models;
 using NLog.Extensions.Logging;
 using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Configure NLog
 builder.Logging.ClearProviders();
 NLog.LogManager.Configuration = new NLogLoggingConfiguration(builder.Configuration.GetSection("NLog"));
 builder.Host.UseNLog();
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.SetIsOriginAllowed(origin => new Uri(origin).IsLoopback)
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//Configure MVC services -> https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.dependencyinjection.mvcservicecollectionextensions.addcontrollers?view=aspnetcore-9.0&viewFallbackFrom=net-8.0
+builder.Services.AddControllers().AddJsonOptions(opts =>
+{
+    var enumConverter = new JsonStringEnumConverter();
+    opts.JsonSerializerOptions.Converters.Add(enumConverter);
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter the token with the `Bearer: ` prefix, e.g. \"Bearer abcde12345\"."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+});
+
 var app = builder.Build();
+//TODO DB migration model
+//TODO rewrite rules if needed
+//TODO add authentication system for user management
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
+var logger = app.Services.GetService<ILogger<Program>>()!;
+app.UseStatusCodePages(context => 
+{
+    var code = context.HttpContext.Response.StatusCode;
+    if (code == 404) {
+        logger.LogError("Resource not found - {Path}",context.HttpContext.Request.Path);
+    }
 
-app.UseStaticFiles();
+    return Task.CompletedTask;
+});
+
+
+app.UseCors();
+//app.UseHttpsRedirection(); TODO
+
 app.UseAntiforgery();
+app.UseStaticFiles();
+app.MapControllers();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
